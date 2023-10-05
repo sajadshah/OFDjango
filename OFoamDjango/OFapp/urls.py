b376ac49489d0499973c22ce6699db55bb777803
sajadshah <shahsavari.sj@gmail.com>
2023-10-05 18:42:57 +0300

    show intermediate images

--- OFoamDjango/OFapp/urls.py
+++ OFoamDjango/OFapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.input_form_view, name='input_form'),
+    path('get-intermediate-images', views.get_intermediate_images, name='get_intermediate_images'),
 ]
